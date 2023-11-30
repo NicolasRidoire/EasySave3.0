b2d3c8a6995c58bcbf7b70c85349cc0b33948637
Alexandre BOCKOUA <alexandre.bockoua@viacesi.fr>
2023-11-30 16:42:17 +0100

    mainpage

---
+++ View/LocalizedStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasySaveV2._0
+{
+    public static class LocalizedStrings
+    {
+        public static string WelcomeMessage => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "WelcomeMessage");
+        public static string BackupInformation => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "BackupInformation");
+        public static string Language => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "language");
+        public static string Delete => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "Delete");
+        public static string Create => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "create");
+        public static string Update => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "update");
+        public static string Execut => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "Execut");
+        public static string Description => LanguageManager.GetLocalizedString(Thread.CurrentThread.CurrentUICulture.Name, "description");
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿// Importez les espaces de noms nécessaires
+using System.Threading;
+using System.Windows;
+
+using System;
+using System.Globalization;  // Assurez-vous que cette ligne est présente
+using System.Windows.Controls;
+using System.ComponentModel;
+
+namespace EasySaveV2._0
+{
+    public partial class MainWindow : Window
+    {
+        private double _infoSaveWidth;
+        public MainWindow()
+        {
+            
+        }
+
+        public double InfoSaveWidth
+        {
+            get { return _infoSaveWidth; }
+            set
+            {
+                if (_infoSaveWidth != value)
+                {
+                    _infoSaveWidth = value;
+                    OnPropertyChanged(nameof(InfoSaveWidth));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string language = ((RadioButton)sender).Tag.ToString();
+
+            ChangeLanguage(language);
+
+            UpdateUI();
+        }
+
+        private void ChangeLanguage(string language)
+        {
+            CultureInfo newCulture = new CultureInfo(language);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+        }
+
+        private void UpdateUI()
+        {
+            welcomeTextBlock.Text = LocalizedStrings.WelcomeMessage;
+            info_save.Text = LocalizedStrings.BackupInformation;
+            Execut.Content = LocalizedStrings.Execut;
+            delete.Content = LocalizedStrings.Delete;
+            update.Content = LocalizedStrings.Update;
+            create.Content = LocalizedStrings.Create;
+            language.Text = LocalizedStrings.Language;
+            description.Text = LocalizedStrings.Description;
+            InfoSaveWidth = (Thread.CurrentThread.CurrentUICulture.Name == "fr-FR") ? 700 : 489;
+        }
+    }
+}
